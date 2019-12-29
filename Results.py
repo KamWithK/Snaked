fcf76a654c7bf0da41b973a7f2c269c1e4663845
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-12-29 03:05:23 +0000

    added graphs

--- Results.py
+++ Results.py
@@ -3,4 +3,25 @@
 
 import TransferLearning
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 model, history = TransferLearning.model, TransferLearning.history
+
+plt.figure(1)
+for c in ["train_loss", "valid_loss"]:
+    plt.plot(history[c], label=c)
+plt.legend()
+plt.xlabel("Epoch")
+plt.ylabel("Average Negative Log Likelihood")
+plt.title("Training and Validation Losses")
+plt.savefig("Training and Validation Losses.png")
+
+plt.figure(2)
+for c in ["train_acc", "valid_acc"]:
+    plt.plot(100 * history[c], label=c)
+plt.legend()
+plt.xlabel("Epoch")
+plt.ylabel("Average Accuracy")
+plt.title("Training and Validation Accuracy")
+plt.savefig("Training and Validation Accuracy")
