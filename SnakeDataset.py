06ec7828959c3687ed69fc6d66a04d20df6ff868
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-12-29 03:06:46 +0000

    simplified imports and SnakeDataset constructor

--- SnakeDataset.py
+++ SnakeDataset.py
@@ -1,24 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import torch, torchvision
-import torch.nn.functional as F
-import os, io, time
-
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as pyplot
-import torch.optim as optim
 
 from PIL import Image
-from torch import nn
-from torchvision import datasets, transforms, models
-from torch.utils.data.sampler import SubsetRandomSampler
-from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from torch.utils.data import Dataset
 
 class SnakeDataset(Dataset):
     # Create/Initialize variables
-    def __init__(self, img_dir, csv_path, transforms=None, test=False):
+    def __init__(self, img_dir, csv_path, transforms=None):
         self.df = pd.read_csv(csv_path, usecols=["scientific_name", "filename"])
         self.csv_path = csv_path
         self.img_dir = img_dir
