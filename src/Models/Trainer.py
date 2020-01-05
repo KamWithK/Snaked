392123792c87edc565485b95bb4bf5112561cfed
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-01-05 02:37:26 +0000

    simplified trainer to not need an extra helper class to track accuracy and loss

--- src/Models/Trainer.py
+++ src/Models/Trainer.py
@@ -7,16 +7,17 @@ import numpy as np
 
 from torch.utils.data import SubsetRandomSampler, DataLoader
 from torchvision import transforms
-from .StatHelper import LossAccuracyKeeper
+from torch.utils.tensorboard import SummaryWriter
 from Data.SnakeDataset import SnakeDataset
 
 # Trains models
 class Trainer():
-    def __init__(self, model, transforms, criterion, optimizer):
-        self.train_loader, self.validation_loader, self.test_loader = self.get_loaders(transforms)
+    def __init__(self, model, transforms, criterion, optimizer, scheduler):
+        self.data_loaders = self.get_loaders(transforms)
         
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model = model
+        self.scheduler = scheduler
         self.model.to(self.device)
 
         self.criterion, self.optimizer = criterion, optimizer
@@ -52,104 +53,83 @@ class Trainer():
         validation_loader = DataLoader(validation_data, sampler=validation_sampler, batch_size=128, num_workers=20)
         test_loader = DataLoader(test_data, sampler=test_sampler, batch_size=128, num_workers=20)
 
-        return train_loader, validation_loader, test_loader
+        data_loaders = {
+            "train": train_loader,
+            "validation": validation_loader
+        }
 
-    def train(self, save_path, max_epoch_stop=5, n_epochs=30, print_every=2):
-        try:
-            print("self.model has been trained for: " + self.model.epochs + " epochs")
-        except:
-            self.model.epochs = 0
-            print("Starting training from scratch")
+        return data_loaders
 
-        overall_start = time.time()
-
-        loss_accuracy_keeper = LossAccuracyKeeper()
+    def train(self, save_folder, n_epochs=30):
+        self.writer = SummaryWriter(save_folder + "/TensorBoard")
+        best_acc = 0.0
 
         for epoch in range(n_epochs):
-            loss_accuracy_keeper.reset()
+            print("Epoch {}/{}:".format(epoch, n_epochs - 1))
+            start_time = time.time()
 
-            epoch_start_time = time.time()
-            self.model.train()
+            for phase in ["train", "validation"]:
+                running_loss = 0.0
+                running_corrects = 0
+                
+                if phase == "train":
+                    self.model.train()
+                else: self.model.eval()
 
-            # Train loop
-            for ii, (data, target) in enumerate(self.train_loader, 0):
-                # Use GPU when available
-                data, target = data.to(self.device), target.to(self.device)
-
-                # Reset gradients
-                self.optimizer.zero_grad()
-                output = self.model(data)
-
-                # Backpropagate
-                loss = self.criterion(output, target)
-                loss.backward()
-
-                # Update prameters
-                self.optimizer.step()
-
-                loss_accuracy_keeper.update_loss_acc(data, target, output, self.criterion, "train", ii, len(self.train_loader))
-
-                # Training progress
-                percent = 100 * (ii + 1) / len(self.train_loader)
-                elapsed_time = time.time() - epoch_start_time
-                print(
-                    f"Epoch: {epoch}\t{percent:.2f}% complete. {elapsed_time:.2f} seconds elapsed",
-                    end="\r"
-                )
-
-            # Validation
-            else:
-                self.model.epochs += 1
-
-                # Don't track gradients
-                with torch.no_grad():
-                    # Evalutaion
-                    self.model.eval()
-
-                    # Validation loop
-                    for ii, (data, target) in enumerate(self.validation_loader, 0):
-                        # Use GPU if available
-                        data, target = data.to(self.device), target.to(self.device)
-                        
-                        # Forward pass
-                        output = self.model(data)
-
-                        loss_accuracy_keeper.update_loss_acc(data, target, output, self.criterion, "validation", ii, len(self.validation_loader))
+                for i, (inputs, labels) in enumerate(self.data_loaders[phase], 0):
+                    progress = i / len(self.data_loaders[phase].sampler)
                     
-                    # Average losses
-                    loss_accuracy_keeper.update_average_loss_acc(len(self.train_loader.dataset))
+                    if i == 0:
+                        time_left, formated_duration = 0, 0
+                    else:
+                        time_left = (100 / progress) * (time.time() - start_time)
+                        formated_duration = time.strftime("%H:%M:%S", time.gmtime(time_left))
 
-                    # Print results
-                    if (epoch + 1) % print_every == 0:
-                        loss_accuracy_keeper.print_progress(epoch)
-                    
-                    # Judges whether improvement has been made
-                    # If not in set max number of epochs, stop early
-                    if loss_accuracy_keeper.safe_improvement(max_epoch_stop) == [True, True]:
-                        state = {
-                            "epoch": epoch,
-                            "state_dict": self.model.state_dict,
-                            "optimizer": self.optimizer.state_dict(),
-                            "validation_loss_min": loss_accuracy_keeper.validation_loss_min,
-                        }
-                        torch.save(state, save_path)
-                        best_epoch = epoch
-                    elif loss_accuracy_keeper.safe_improvement(max_epoch_stop) == [False, False]:
-                        # Load state dict
-                        #self.model.load_state_dict(torch.load(save_path))
-                        print("Exiting early, as there has been no recent improvements")
-                        break
+                    print(f"Phase: {phase}      Progress: {progress}%       Time Left: +{formated_duration}", end="\r")
 
-        self.model.optimizer = self.optimizer
-        total_time = time.time() - overall_start
-        validation_acc = loss_accuracy_keeper.acc["validation"]
-        print(
-            f"\nBest epoch: {best_epoch} with loss: {loss_accuracy_keeper.validation_loss_min:.2f} and accuracy: {100 * validation_acc:.2f}%"
-        )
-        print(
-            f"{total_time:.2f} total seconds elapsed. {total_time / (epoch + 1):.2f} seconds per epoch"
-        )
-        return self.model, loss_accuracy_keeper.get_history_dataframe()
+                    inputs, labels = inputs.to(self.device), labels.to(self.device)
+
+                    self.optimizer.zero_grad()
+
+                    with torch.set_grad_enabled(phase == "train"):
+                        outputs = self.model(inputs)
+                        _, preds = torch.max(outputs, 1)
+                        loss = self.criterion(outputs, labels)
+
+                        if phase == "train":
+                            loss.backward()
+                            self.optimizer.step()
+
+                    running_loss += loss.item() * inputs.size(0)
+                    running_corrects += torch.sum(preds == labels.data)
+                
+                epoch_loss = running_loss / len(self.data_loaders[phase].sampler)
+                epoch_acc = running_corrects.double() / len(self.data_loaders[phase].sampler)
+                epoch_time = time.time() - start_time
+                
+                if phase == "validation":
+                    self.scheduler.step(epoch_loss)
+
+                self.writer.add_scalar(phase + "/loss", epoch_loss, epoch)
+                self.writer.add_scalar(phase + "/acc", epoch_acc, epoch)
+
+                self.writer.flush()
+
+                print("\nPhase: {}, Loss: {:.4f}, Acc: {:.4f}, Time: {:.4f}".format(phase, epoch_loss, epoch_acc, epoch_time))
+
+                if phase == "validation" and epoch_acc > best_acc:
+                    best_acc = epoch_acc
+                    torch.save({
+                        "epoch": epoch,
+                        "acc": epoch_acc,
+                        "loss": epoch_loss,
+                        "model_state_dict": self.model.state_dict(),
+                        "optimizer_state_dict": self.optimizer.state_dict(),
+                        "scheduler_state_dict": self.scheduler.state_dict()
+                    }, save_folder + "/Model.tar")
+                
+            print()
+        return self.model
 
     def load(self, model, path, trainning=False):
         checkpoint = torch.load(path)
